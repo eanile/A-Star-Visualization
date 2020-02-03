4b99df896943f2b02d19ac05b959adf80e8f4516
Eliano Anile <elianoanile@gmail.com>
2020-02-02 21:14:43 -0500

    Renamed a few functions

--- window.py
+++ window.py
@@ -42,12 +42,54 @@ class Window:
 
         # Left mouse button should create an obstacle, right mouse button
         # should erase an obstacle.
-        self.canvas.bind('<B1-Motion>', self._draw_obstacle)
-        self.canvas.bind('<Button-1>', self._draw_obstacle)
-        self.canvas.bind('<B3-Motion>', self._erase_obstacle)
-        self.canvas.bind('<Button-3>', self._erase_obstacle)
+        self.canvas.bind('<B1-Motion>', self.__draw_obstacle)
+        self.canvas.bind('<Button-1>', self.__draw_obstacle)
+        self.canvas.bind('<B3-Motion>', self.__erase_obstacle)
+        self.canvas.bind('<Button-3>', self.__erase_obstacle)
 
-    def _box_to_corner_coords(self, x: int, y: int) -> Tuple[Tuple, Tuple]:
+    def __draw_obstacle(self, event: tk.Event) -> None:
+        """ Draw an obstacle (in the form of a filled in square) on the screen.
+
+        Args:
+            event {tkinder.Event}: details from the triggered event
+
+        Returns:
+            None
+        """
+        # Get the box that the user clicked.
+        box_x, box_y = self.abs_to_box(event.x, event.y)
+
+        # No need to draw the obstacle if it already exists
+        if (box_x, box_y) in self.obstacles.values():
+            return
+
+        # Compute corners of box and draw a rectangle on the canvas
+        top_left, bottom_right = self.box_to_corner_coords(box_x, box_y)
+
+        id = self.canvas.create_rectangle(top_left, bottom_right, fill='black')
+        self.canvas.pack()
+        self.obstacles[id] = box_x, box_y
+
+    def __erase_obstacle(self, event: tk.Event) -> None:
+        """ Erase an obstacle (in the form of a filled in square) on the screen.
+
+        Args:
+            event {tkinder.Event}: details from the triggered event
+
+        Returns:
+            None
+        """
+        # Get the IDs of all items overlapping where the user clicked.
+        ids = self.canvas.find_overlapping(event.x, event.y, event.x, event.y)
+
+        # Make sure there is only one item where the user clicked (looking for
+        # a box) and that the item is in the obstacles list (as it's possible
+        # the user clicked on a line that defines the grid).
+        if len(ids) == 1 and ids[0] in self.obstacles.keys():
+            self.canvas.delete(ids[0])
+            del self.obstacles[ids[0]]
+
+    def box_to_corner_coords(self, x: int, y: int) -> Tuple[Tuple, Tuple]:
         """ Converts a box's coordinates to the coordinates of it's top left
         and bottom right corners on the canvas.
 
@@ -66,7 +108,7 @@ class Window:
 
         return top_left, bottom_right
 
-    def _abs_to_box(self, x: int, y: int) -> Tuple:
+    def abs_to_box(self, x: int, y: int) -> Tuple:
         """ Converts absolute coordinates where the user clicked to the box in
         which they clicked.
 
@@ -81,48 +123,6 @@ class Window:
         box_y = y // self.box_height
 
         return box_x, box_y
-
-    def _draw_obstacle(self, event: tk.Event) -> None:
-        """ Draw an obstacle (in the form of a filled in square) on the screen.
-
-        Args:
-            event {tkinder.Event}: details from the triggered event
-
-        Returns:
-            None
-        """
-        # Get the box that the user clicked.
-        box_x, box_y = self._abs_to_box(event.x, event.y)
-
-        # No need to draw the obstacle if it already exists
-        if (box_x, box_y) in self.obstacles.values():
-            return
-
-        # Compute corners of box and draw a rectangle on the canvas
-        top_left, bottom_right = self._box_to_corner_coords(box_x, box_y)
-
-        id = self.canvas.create_rectangle(top_left, bottom_right, fill='black')
-        self.canvas.pack()
-        self.obstacles[id] = box_x, box_y
-
-    def _erase_obstacle(self, event: tk.Event) -> None:
-        """ Erase an obstacle (in the form of a filled in square) on the screen.
-
-        Args:
-            event {tkinder.Event}: details from the triggered event
-
-        Returns:
-            None
-        """
-        # Get the IDs of all items overlapping where the user clicked.
-        ids = self.canvas.find_overlapping(event.x, event.y, event.x, event.y)
-
-        # Make sure there is only one item where the user clicked (looking for
-        # a box) and that the item is in the obstacles list (as it's possible
-        # the user clicked on a line that defines the grid).
-        if len(ids) == 1 and ids[0] in self.obstacles.keys():
-            self.canvas.delete(ids[0])
-            del self.obstacles[ids[0]]
 
     def activate(self):
         self.root.mainloop()
