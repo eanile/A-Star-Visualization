0d179af312071c071490e1d95fc9b05375789853
Eliano Anile <elianoanile@gmail.com>
2020-02-04 20:15:35 -0500

    Refactored some code / check if a user clicked a box out of bounds.

--- window.py
+++ window.py
@@ -2,6 +2,9 @@ import tkinter as tk
 
 from typing import Dict, Tuple
 
+# Type definitions.
+Coordinate = Tuple[int, int]
+
 
 class Window:
     """ This class defines all window-related behaviour, including updating the
@@ -31,9 +34,9 @@ class Window:
         """
         # Create vertical and horizontal lines that define the grid and draw
         # them on the canvas.
-        for x in range(0, self.width, self.box_width):
+        for x in range(0, self.width + 1, self.box_width):
             self.canvas.create_line([x, 0], [x, self.height])
-        for y in range(0, self.height, self.box_height):
+        for y in range(0, self.height + 1, self.box_height):
             self.canvas.create_line([0, y], [self.width, y])
         self.canvas.pack()
 
@@ -54,18 +57,19 @@ class Window:
     def __draw_obstacle(self, event: tk.Event) -> None:
         """ Draw an obstacle (a filled square) on the screen."""
         # Get the coordinates of the box that the user clicked.
-        box_x, box_y = self.abs_to_box_coords(event.x, event.y)
+        coords = self.abs_to_box_coords(event.x, event.y)
 
-        # No need to draw the obstacle if it already exists
-        if (box_x, box_y) in self.obstacles.values():
+        # No need to draw the obstacle if it already exists or if the user
+        # clicked in an area outside the grid.
+        if coords in self.obstacles.values() or self.out_of_bounds(coords):
             return
 
         # Compute corners of box and draw a rectangle on the canvas
-        top_left, bottom_right = self.box_to_corner_coords(box_x, box_y)
+        top_left, bottom_right = self.box_to_corner_coords(coords)
 
         id = self.canvas.create_rectangle(top_left, bottom_right, fill='black')
         self.canvas.pack()
-        self.obstacles[id] = box_x, box_y
+        self.obstacles[id] = coords
 
     def __erase_obstacle(self, event: tk.Event) -> None:
         """ Erase an obstacle (a filled square) on the screen."""
@@ -79,13 +83,14 @@ class Window:
             self.canvas.delete(ids[0])
             del self.obstacles[ids[0]]
 
-    def box_to_corner_coords(self, x: int, y: int) -> Tuple[Tuple[int, int],
-                                                            Tuple[int, int]]:
+    def box_to_corner_coords(self, box_coords: Coordinate) -> Tuple[Coordinate,
+                                                                    Coordinate]:
         """ Converts a box's coordinates to the coordinates of it's top left
         and bottom right corners on the canvas.
         """
-        top_left = (x * self.box_width,
-                    y * self.box_height)
+
+        top_left = (box_coords[0] * self.box_width,
+                    box_coords[1] * self.box_height)
         bottom_right = (top_left[0] + self.box_width,
                         top_left[1] + self.box_height)
 
@@ -96,3 +101,12 @@ class Window:
         in which they clicked.
         """
         return x // self.box_width, y // self.box_height
+
+    def out_of_bounds(self, box_coords: Coordinate) -> bool:
+        """ Return whether the coordinates of a box that the user clicked is
+        outside the bounds of the grid.
+        """
+        if box_coords[0] > self.width // self.box_width - 1 or \
+           box_coords[1] > self.height // self.box_height - 1:
+            return True
+        return False
