c5f2d77aff9b9d7a57c7186a12c97fe841a0c1a4
Eliano Anile <elianoanile@gmail.com>
2020-02-04 19:43:40 -0500

    Refactored some code comments / adjusted type hints.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sys
 import window
 
 
-def main() -> None:
+def main() -> int:
     """ Entry point of the program. """
     win_root = window.Window()
     win_root.activate()
+    return 0
 
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from typing import Tuple
+from typing import Dict, Tuple
 
 
 class Window:
@@ -8,36 +8,33 @@ class Window:
     window with the algorithm's progress and allowing the user to create and
     destroy obstacles before running the algorithm.
 
-    Args:
-        width {int}: width of new window grid.
-        height {int}: height of new window grid.
+    Class Variables:
+        box_width: Width of a box in the grid.
+        box_height: Height of a box in the grid.
+        obstacles: Dictionary of tkinter ID to box coordinates.
     """
-    box_width = 25   # Width of a box in the grid.
-    box_height = 25  # Height of a box in the grid.
-    obstacles = {}   # Dictionary of tkinter ID to box coordinates.
+    box_width: int = 25
+    box_height: int = 25
+    obstacles: Dict[int, Tuple[int, int]] = {}
 
     def __init__(self, width: int = 500, height: int = 500) -> None:
-        """ Initialize the window, including drawing the grid and registering
-        callback functions for when a user clicks the screen.
-
-        Args:
-            width {int}: width of the new window grid.
-            height {int}: height of new window grid.
-
-        Returns:
-            None.
-        """
         self.root = tk.Tk()
         self.width = width
         self.height = height
         self.canvas = tk.Canvas(self.root, width=width, height=height)
 
+        self.__create_window()
+
+    def __create_window(self) -> None:
+        """ Draw the grid and register callback functions for when a user
+        clicks the screen.
+        """
         # Create vertical and horizontal lines that define the grid and draw
         # them on the canvas.
-        for x in range(0, width, self.box_width):
-            self.canvas.create_line([x, 0], [x, height])
-        for y in range(0, height, self.box_height):
-            self.canvas.create_line([0, y], [width, y])
+        for x in range(0, self.width, self.box_width):
+            self.canvas.create_line([x, 0], [x, self.height])
+        for y in range(0, self.height, self.box_height):
+            self.canvas.create_line([0, y], [self.width, y])
         self.canvas.pack()
 
         button = tk.Button(self.root, text="Start A*", height=2, width=10)
@@ -51,16 +48,9 @@ class Window:
         self.canvas.bind('<Button-3>', self.__erase_obstacle)
 
     def __draw_obstacle(self, event: tk.Event) -> None:
-        """ Draw an obstacle (in the form of a filled in square) on the screen.
-
-        Args:
-            event {tkinter.Event}: details from the triggered event
-
-        Returns:
-            None
-        """
-        # Get the box that the user clicked.
-        box_x, box_y = self.abs_to_box(event.x, event.y)
+        """ Draw an obstacle (a filled square) on the screen."""
+        # Get the coordinates of the box that the user clicked.
+        box_x, box_y = self.abs_to_box_coords(event.x, event.y)
 
         # No need to draw the obstacle if it already exists
         if (box_x, box_y) in self.obstacles.values():
@@ -74,14 +64,7 @@ class Window:
         self.obstacles[id] = box_x, box_y
 
     def __erase_obstacle(self, event: tk.Event) -> None:
-        """ Erase an obstacle (in the form of a filled in square) on the screen.
-
-        Args:
-            event {tkinter.Event}: details from the triggered event
-
-        Returns:
-            None
-        """
+        """ Erase an obstacle (a filled square) on the screen."""
         # Get the IDs of all items overlapping where the user clicked.
         ids = self.canvas.find_overlapping(event.x, event.y, event.x, event.y)
 
@@ -92,17 +75,10 @@ class Window:
             self.canvas.delete(ids[0])
             del self.obstacles[ids[0]]
 
-    def box_to_corner_coords(self, x: int, y: int) -> Tuple[Tuple, Tuple]:
+    def box_to_corner_coords(self, x: int, y: int) -> Tuple[Tuple[int, int],
+                                                            Tuple[int, int]]:
         """ Converts a box's coordinates to the coordinates of it's top left
         and bottom right corners on the canvas.
-
-            Args:
-                x {int}: x coordinate of the box.
-                y {int}: y coordinate of the box.
-
-            Returns:
-                Tuple[int, int]: top_left corner of the box.
-                Tuple[int, int]: bottom_right corner of the box.
         """
         top_left = (x * self.box_width,
                     y * self.box_height)
@@ -111,21 +87,11 @@ class Window:
 
         return top_left, bottom_right
 
-    def abs_to_box(self, x: int, y: int) -> Tuple:
-        """ Converts absolute coordinates where the user clicked to the box in
-        which they clicked.
-
-            Args:
-                x {int}: x coordinate of the user's click.
-                y {int}: y coordinate of the user's click.
-
-            Returns:
-                Tuple[int, int]: coordinates of the box.
+    def abs_to_box_coords(self, x: int, y: int) -> Tuple[int, int]:
+        """ Converts the absolute coordinates where the user clicked to the box
+        in which they clicked.
         """
-        box_x = x // self.box_width
-        box_y = y // self.box_height
-
-        return box_x, box_y
+        return x // self.box_width, y // self.box_height
 
     def activate(self):
         self.root.mainloop()
