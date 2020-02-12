7ac99c808519f7a538e6864f9e6043303f037678
Eliano Anile <elianoanile@gmail.com>
2020-02-11 22:03:02 -0500

    Create a Node class to keep track of helpful values during the pathfinding process.

--- pathfinding.py
+++ pathfinding.py
@@ -1,9 +1,35 @@
+import heapq
+import math
 import window
 
 from typing import List
 
 # Type definitions.
 Cell_ID = int
+
+
+class Node:
+    """ Holds important information about a Cell when calculating the shortest
+    path.
+        Notes:
+        f_score = g_score + h_score
+        g_score: exact time to get from the start to the current node
+        h_score: heuristic estimate of the time to get from the current node
+                 to the end
+        came_from is used to construct the path when the algorithm is complete
+    """
+    came_from: Cell_ID
+    g_score: float
+    f_score: float
+    visited: bool
+
+    def __init__(self):
+        # Initialize g_score to infinity so any newly discovered node will have
+        # a g_score less than the initialized one.
+        self.came_from = None
+        self.g_score = math.inf
+        self.f_score = math.inf
+        self.visited = False
 
 
 def a_star(window: window.Window) -> List[Cell_ID]:
--- start.py
+++ start.py
@@ -2,10 +2,11 @@ import sys
 import window
 
 
-def main() -> None:
+def main() -> int:
     """ Entry point of the program. """
     main_window = window.Window()
     main_window.root.mainloop()
+    return 0
 
 
 if __name__ == "__main__":
